a6cc0d947c8552d0536c8e20efda4a4a65d4e857
Milad <8590410@gmail.com>
2019-03-23 15:38:03 +0430

    show Exit results to index

--- eoth2.js
+++ eoth2.js
@@ -1,4 +1,5 @@
 function eoth2() {
+    var resultCss = document.getElementById('result').classList;
 
     var today = new Date();
     //get Start Date
@@ -20,12 +21,30 @@ function eoth2() {
     exitMiladiDate.setYear(exitMiladi[0]);
 
     var nowDate = new Date();
-
+    var tarkhis="";
     console.log(exitMiladiDate, nowDate);
-    console.log(date_diff_indays(nowDate, exitMiladiDate));
-    console.log(date_diff_inMonths(nowDate, exitMiladiDate));
-    console.log(date_diff_inHours(nowDate, exitMiladiDate));
-    console.log(date_diff_inMinutes(nowDate, exitMiladiDate));
+    var remainDays = date_diff_indays(nowDate, exitMiladiDate);
+    var remainWeeks = date_diff_inWeeks(nowDate, exitMiladiDate);
+    var remainMonths = date_diff_inMonths(nowDate, exitMiladiDate);
+    var remainHours =date_diff_inHours(nowDate, exitMiladiDate);
+    var remainMinutes =date_diff_inMinutes(nowDate, exitMiladiDate);
+
+
+
+        resultCss.add('alert-secondary', 'border');
+        //print in result div
+        document.getElementById("result").innerHTML =
+            tarkhis +
+            // "  روز خدمت کرده " + "   &emsp; :   &emsp;" + goneDay +
+            // "<br>" + "  هفته خدمت کرده " + "   &emsp; :   &emsp;" + goneWeek.toFixed(1) +
+            "<br>" + "  روز تا ترخیص" + "   &emsp; :   &emsp;" + remainDays +
+            "<br>" + "  هفته تا ترخیص" + "   &emsp; :   &emsp;" + remainWeeks +
+            "<br>" + "  ماه تا ترخیص" + "   &emsp; :   &emsp;" + remainMonths +
+            "<br>" + "  ساعت تا ترخیص" + "   &emsp; :   &emsp;" + remainHours+
+            "<br>" + "  دقیقه تا ترخیص" + "   &emsp; :   &emsp;" + remainMinutes;
+
+
+
 }
 
 
@@ -35,6 +54,11 @@ var date_diff_indays = function (date1, date2) {
     return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) / (1000 * 60 * 60 * 24));
 };
 
+var date_diff_inWeeks = function (date1, date2) {
+    dt1 = new Date(date1);
+    dt2 = new Date(date2);
+    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) / (1000 * 60 * 60 * 24 * 7));
+};
 var date_diff_inMonths = function (date1, date2) {
     dt1 = new Date(date1);
     dt2 = new Date(date2);
